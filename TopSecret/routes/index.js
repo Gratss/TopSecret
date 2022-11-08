a0e666cd3a817b1cc4f05e335ed368ede5e0f55c
Gratss <snaelzzz@gmail.com>
2022-11-09 01:03:21 +0300

    4.2 TopSecretWork

--- TopSecret/routes/index.js
+++ TopSecret/routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/R-32', function(req, res, next) {
+  res.send("<h1>Страница TopSecret Nissan GT-R R32</h1>")
 });
+router.get('/R-33', function(req, res, next) {
+  res.send("<h1>Страница TopSecret Nissan GT-R R33</h1>")
+});
+router.get('/SupraA70', function(req, res, next) {
+  res.send("<h1>Страница TopSecret Toyota SupraA70</h1>")
+});
+
+
 
 module.exports = router;
