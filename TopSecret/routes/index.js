e8ed6d81d51e50936658dc0b09894a69b946cb00
Gratss <snaelzzz@gmail.com>
2022-11-09 01:39:25 +0300

    4.3 TopSecretWork

--- TopSecret/routes/index.js
+++ TopSecret/routes/index.js
@@ -3,13 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/R-32', function(req, res, next) {
-  res.send("<h1>Страница TopSecret Nissan GT-R R32</h1>")
+  res.render('Car', {
+      title: "Nissan GT-R R32",
+      picture: "R-32.jpg",
+      desc: "Компания Top Secret Japan, показала восстановленный R32, на шикарных дисках и с обвесом. Диффузоры, карнары, множество карбона и олдскульный вид не испорчен!."
+  });
 });
+
 router.get('/R-33', function(req, res, next) {
-  res.send("<h1>Страница TopSecret Nissan GT-R R33</h1>")
+  res.render('Car', {
+      title: "Nissan GT-R R33",
+      picture: "R-33.jpg",
+      desc: "Компания Top Secret Japan показала GT-R R33 цвета шампанского, на шикарных дисках и с обвесом, ~1200 лошадинных сил под капотом."
+  });
 });
 router.get('/SupraA70', function(req, res, next) {
-  res.send("<h1>Страница TopSecret Toyota SupraA70</h1>")
+  res.render('Car', {
+      title: "Toyota SupraA70",
+      picture: "SupraA70.jpg",
+      desc: "Top Secret Japan представила нам Toyota Supra.A70 цвета шампанского, ставшего уже легендарным. Мощность выше 1000 лошадинных сил на 5500 об/мин.На автомагистрали А1 гонщик мчал со скоростью 317 км/ч (197 миль в час)"
+  });
 });
 
 
