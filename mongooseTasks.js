75de25f89381dc0ece716e775c782a8e91cb9f9f
Gratss <snaelzzz@gmail.com>
2022-11-23 19:55:18 +0300

    7.2

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,13 +1,15 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 
-var Car = mongoose.model('Car', { name: String })
+var schema = mongoose.Schema({ name: String })
 
-var jdm = new Car({ name: 'Supra' })
+schema.methods.meow = function(){
+    console.log(this.get("name") + " done 'Strufustfu' ")
+}
+
+var Car = mongoose.model('Car', schema)
+
+var jdm = new Car({ name: 'SupraA80' })
 jdm.save(function (err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('Sututututu')
-    }
+    jdm.meow()
 })
