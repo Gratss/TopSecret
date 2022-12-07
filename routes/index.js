68ef7647e21c440510ee0b46c766965793a77b52
Gratss <snaelzzz@gmail.com>
2022-12-07 22:28:07 +0300

    9.2

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,24 @@ var router = express.Router();
 var Car = require("../models/car").Car
 
 
+
 router.get('/', function(req, res, next) {
-  Car.find({},{_id:0,title:1,nick:1},function(err,menu){
-      res.render('index', {
-                              title: 'Express',
-                              menu: menu
-                          });
-  })
+    Car.find({},{_id:0,title:1,nick:1},function(err,menu){
+        res.render('index', {
+                                title: 'Express',
+                                menu: menu
+                            });
+    })
 
 });
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.cookie('greeting', 'Hellooo!!!').render('index', { title: 'Express', menu:menu });
+});
+
+
+module.exports = router;
 /* GET home page. 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
