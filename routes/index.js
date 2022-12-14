1552982dd69be8a93ba2f1e7fd8decb0ea3c7fbf
Gratss <snaelzzz@gmail.com>
2022-12-14 23:14:39 +0300

    10.4

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
-
 var express = require('express');
 var router = express.Router();
 var Car = require("../models/car").Car
+var User = require("./../models/user").User
 
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   Car.find({}, { _id: 0, title: 1, nick: 1 }, function (err, menu) {
-    req.session.greeting = "HelllllllllloWorld!",
-      res.cookie('greeting', 'HellllllllloWorld!').render('index', {
+    req.session.greeting = "HelllllllllllloWorld!",
+      res.cookie('greeting', 'HelllllllllloWorld!').render('index', {
         title: 'Express',
         menu: menu,
         counter:req.session.counter
@@ -23,12 +23,31 @@ router.get('/logreg', function(req, res, next) {
   });
 
   /* POST login/registration page. */
-router.post('/logreg', function (req, res, next) {
-  var username = req.body.username
-  var password = req.body.password
+  router.post('/logreg', function(req, res, next) {
+    var username = req.body.username
+    var password = req.body.password
+    User.findOne({username:username},function(err,user){
+        if(err) return next(err)
+        if(user){
+            if(user.checkPassword(password)){
+                req.session.user = user._id
+                res.redirect('/')
+            } else {
+                      res.render('logreg', {title: 'Вход'})
+            }
+       } else {
+       var user = new User({username:username,password:password})
+            user.save(function(err,user){
+                if(err) return next(err)
+                req.session.user = user._id
+                res.redirect('/')
+            })        
+      }
+    })
 });
 
-module.exports = router;;
+
+module.exports = router;
 /* GET home page. 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
