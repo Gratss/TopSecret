065750e4892d0bdae393979f178da22ec44293f6
Gratss <snaelzzz@gmail.com>
2022-12-21 21:49:04 +0300

    10.8

--- routes/cars.js
+++ routes/cars.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var Car = require("../models/car").Car
+var checkAuth = require("./../middleware/checkAuth.js")
 var async = require("async")
 
 /* GET users listing. */
@@ -12,27 +13,17 @@ router.get('/', function(req, res, next) {
 /* Страница машин */
 
 
-router.get('/:nick', function(req, res, next) {
-    async.parallel([
-            function(callback){
-                Car.findOne({nick:req.params.nick}, callback)
-            },
-            function(callback){
-                Car.find({},{_id:0,title:1,nick:1},callback)
-            }
-        ],
-        function(err,result){
-            if(err) return next(err)
-            var car = result[0]
-            var cars = result[1] || []
-            if(!car) return next(new Error("There was no such cаr in TopSecret"))
-            res.render('car', {
-                title: car.title,
-                picture: car.avatar,
-                desc: car.desc,
-                menu: cars
-            });
+router.get('/:nick', checkAuth, function (req, res, next) {
+    Car.findOne({ nick: req.params.nick }, function (err, car) {
+        if (err) return next(err)
+        if (!car) return next(new Error("Не найдено такой машины"))
+        res.render('car', {
+            title: car.title,
+            picture: car.avatar,
+            desc: car.desc
         })
-  })
-  
-  module.exports = router
+    })
+})
+
+
+module.exports = router
