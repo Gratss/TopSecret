20493f92c570a0b5067a5fabd367cade971cfb58
Gratss <snaelzzz@gmail.com>
2022-12-21 21:34:08 +0300

    10.5

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/topsecret')
+mongoose.connect('mongodb://localhost/TopSecret')
 var session = require("express-session")
-var cars = require('./routes/cars');
+var topsecret = require("./models/topsecret").topsecret
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var topsecret = require('./routes/topsecret');
 
 var app = express();
 
 // view engine setup
-app.engine('ejs',require('ejs-locals'));
+app.engine('ejs', require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -27,41 +27,47 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var MongoStore = require('connect-mongo'); (session);
 app.use(session({
-  secret: "Jdm",
-  cookie:{maxAge:60*1000},
+  secret: "TopSecret",
+  cookie: { maxAge: 60 * 1000 },
   resave: true,
-  saveUninitialized: true	,
-  store: MongoStore.create({mongoUrl: 'mongodb://127.0.0.1:27017/topsecret'})
+  saveUninitialized: true,
+  store: MongoStore.create({ mongoUrl: 'mongodb://localhost/TopSecret' })
 }))
-app.use(function(req,res,next){
-  req.session.counter = req.session.counter +1 || 1
+app.use(function (req, res, next) {
+  req.session.counter = req.session.counter + 1 || 1
   next()
 })
 
+app.use(function (req, res, next) {
+  res.locals.nav = []
+  Avocado.find(null, { _id: 0, title: 1, nick: 1 }, function (err, result) {
+    if (err) throw err
+    res.locals.nav = result
+    next()
+  })
+})
+
+app.use(require("./createMenu.js"))
+app.use(require("./createUser.js"))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/cars', cars);
+app.use('/avocado', avocado);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error',
-  {
-    picture: "../images/error.png",
-    title: 'Ошибка, проверьте правильность введённых данных.',
-    menu: []
-  });
+  res.render('error', { title: " Упс ", menu: [] });
 });
 
 module.exports = app;
