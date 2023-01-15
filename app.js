28ad94a9f5caef104ad5cb9a35603b9ef00f421b
Gratss <snaelzzz@gmail.com>
2023-01-15 23:49:27 +0300

    ses, 11.5

--- app.js
+++ app.js
@@ -3,19 +3,34 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/TopSecret')
+//var mongoose = require('mongoose')
+//mongoose.connect('mongodb://127.0.0.1:27017/topsecret')
 var session = require("express-session")
-var topsecret = require("./models/topsecret").topsecret
+var mysql2 = require('mysql2/promise');
+var MySQLStore = require('express-mysql-session')(session);
+
+
+var cars = require('./routes/cars');
+var Car = require("./models/car").Car
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var topsecret = require('./routes/topsecret');
 
 var app = express();
 
+
+var options = {
+  host : '127.0.0.1',
+  port: '3306',
+  user : 'root',
+  password : 'qwas233',
+  database: 'topsecret'
+  };
+  var connection = mysql2.createPool(options)
+  var sessionStore = new MySQLStore( options, connection);
+
 // view engine setup
-app.engine('ejs', require('ejs-locals'));
+app.engine('ejs',require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -25,49 +40,68 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var MongoStore = require('connect-mongo'); (session);
+//var MongoStore = require('connect-mongo'); (session);
+//app.use(session({
+//  secret: "JdmCars",
+//  cookie:{maxAge:60*1000},
+//  resave: true,
+//  saveUninitialized: true	,
+ // store: MongoStore.create({mongoUrl: 'mongodb://127.0.0.1:27017/topsecret'})
+//}))
 app.use(session({
-  secret: "TopSecret",
-  cookie: { maxAge: 60 * 1000 },
+  secret: 'TopSecret',
+  key: 'sid',
+  store: sessionStore,
   resave: true,
   saveUninitialized: true,
-  store: MongoStore.create({ mongoUrl: 'mongodb://localhost/TopSecret' })
-}))
-app.use(function (req, res, next) {
-  req.session.counter = req.session.counter + 1 || 1
+  cookie: { path: '/',
+  httpOnly: true,
+  maxAge: 60*1000
+  }
+  }));
+  
+
+
+app.use(function(req,res,next){
+  req.session.counter = req.session.counter +1 || 1
   next()
 })
 
-app.use(function (req, res, next) {
+app.use(function(req,res,next){
   res.locals.nav = []
-  Avocado.find(null, { _id: 0, title: 1, nick: 1 }, function (err, result) {
-    if (err) throw err
-    res.locals.nav = result
-    next()
+
+  Car.find(null,{_id:0,title:1,nick:1},function(err,result){
+      if(err) throw err
+      res.locals.nav = result
+      next()
   })
 })
 
-app.use(require("./createMenu.js"))
-app.use(require("./createUser.js"))
 
+app.use(require("./middleware/createUser.js"));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/avocado', avocado);
+app.use('/cars', cars);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', { title: " Упс ", menu: [] });
+  res.render('error',
+  {
+    picture: "../images/error.png",
+    title: 'Ошибка!',
+    menu: []
+  });
 });
 
 module.exports = app;
